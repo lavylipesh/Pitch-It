30a2d12cb1a2d2b1d8e2f22ea7e113970d83b727
lavylipesh <lilibethlangat@yahoo.com>
2019-09-15 18:55:06 -0400

    add navbar and css to application

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-return app
+
+from app import views
+
+
+
--- manage.py
+++
@@ -1,1 +0,0 @@
-from flask_script import Manager,Server
