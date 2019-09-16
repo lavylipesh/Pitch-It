983cebb18621d65e13dd8655acaaf6eb4ab77166
lavylipesh <lilibethlangat@yahoo.com>
2019-09-16 08:31:44 -0400

    add create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
-from app import views
-
+bootstrap = Bootstrap()
+db = SQLAlchemy(app)
 
 
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    return app
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://lilibeth:1234@localhost/pitch'
+    SECRET_KEY = '2wnd56mdj6hcmnc7cxn'
